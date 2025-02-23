3d2692bc185def4014a8a9c41b968de4df80763c
Mabongakev <kelvin.mabonga92@gmail.com>
2025-02-23 10:07:18 -0800

    Task 1 updated

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -39,7 +39,8 @@ def hbnb():
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places, cache_id=uuid.uuid4())
+                           places=places, 
+                           cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
